de158432d2a66ac0687651431e0e64a99d2d959a
nathansetyawan96 <nathan.setyawan96@gmail.com>
2021-12-18 13:03:11 +0800

    gd blog seperated into 3 parts

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
   return (
     <Layout itemScope itemType="http://schema.org/NotFound">
       <Seo title="404: Not Found" type="website" slug="/404/" />
-      <h1>404: Not found, try again</h1>
+      <h1>404: Not found, try again.</h1>
     </Layout>
   )
 }
