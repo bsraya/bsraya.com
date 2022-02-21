c3e28629ab0e861b0946a3226286d387b6b641fd
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-02-22 02:13:45 +0800

    gif file removed, changed to giphy embed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import scream from "../assets/scream.gif"
 
 export default function NotFound() {
   return (
     <Layout itemScope itemType="http://schema.org/NotFound">
       <Seo title="404: Not Found" type="website" slug="/404/" />
       <h1>404: Not found, try again.</h1>
-      <img src={scream} alt="Screaming beaver" height={500} width={500}/>
+      <iframe title="Screaming beaver" src="https://giphy.com/embed/ZDOXnhXvESwSY" width="480" height="405" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/critters-beavers-ZDOXnhXvESwSY">via GIPHY</a></p>
     </Layout>
   )
 }
