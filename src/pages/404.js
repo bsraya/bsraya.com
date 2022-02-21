4a50367d9f99b95e431c2c81c110cd198db6fdbe
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-02-22 02:09:20 +0800

    404 page comes with a gif

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import scream from "../assets/scream.gif"
 
 export default function NotFound() {
   return (
     <Layout itemScope itemType="http://schema.org/NotFound">
       <Seo title="404: Not Found" type="website" slug="/404/" />
       <h1>404: Not found, try again.</h1>
+      <img src={scream} alt="Screaming beaver" height={500} width={500}/>
     </Layout>
   )
 }
