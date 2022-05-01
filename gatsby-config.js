285a2b630d8b0a9cbd18c98fd44b5c3a925b628c
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-05-01 21:22:49 +0800

    Update resume link

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     email: `nathan.setyawan96@gapp.nthu.edu.tw`,
     siteUrl: `https://www.bsraya.com`,
-    resume: `https://drive.google.com/file/d/1i_MoSHg68ZPNVwrtWHs1iqkH1jabmYt3/view?usp=sharing`,
+    resume: `https://drive.google.com/file/d/1MVkMj3jRxBpQ4dVJfiTkhuvTvcUsa-pG/view?usp=sharing`,
     menuLinks: [
       {
         name: 'Home',
